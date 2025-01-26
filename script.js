29459dbe2e7461dcc8c8a45dc3b578d69d7591a4
jhonatan583 <jhoninhaoficial@gmail.com>
2025-01-26 17:15:03 -0300

    add Libra e Bitcoin

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ function convertValues() {
    const currencyValueToConverted = document.querySelector(".currency-value")
 
 
-   const dolartoday = 6.02
-   const eurotoday = 6.24
+   const dolartoday = 5.91
+   const eurotoday = 6.19
+   const libratoday = 7.38
+   const bitcointoday = 628711
 
 
-   if (currencyselect.value == "dolar") {
+   if (currencyselect.value == "Dólar") {
       currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
          style: "currency",
          currency: "USD"
@@ -19,13 +21,27 @@ function convertValues() {
 
    }
 
-   if (currencyselect.value == "euro") {
+   if (currencyselect.value == "Euro") {
       currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
          style: "currency",
          currency: "EUR"
       }).format(inputCurrencyValue / eurotoday)
    }
 
+   if (currencyselect.value == "Libra") {
+      currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+         style: "currency",
+         currency: "GBP"
+      }).format(inputCurrencyValue / libratoday)
+
+   }
+
+   if (currencyselect.value == "Bitcoin") {
+      currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+         style: "currency",
+         currency: "BTC"
+      }).format(inputCurrencyValue / bitcointoday)
+   }
    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
       style: "currency",
       currency: "BRL"
@@ -36,14 +52,25 @@ function changeCurrency() {
    const currencyname = document.getElementById("currency-name")
    const currencyimage = document.querySelector(".currency-image")
 
-   if (currencyselect.value == "dolar") {
-      currencyname.innerHTML = "dolar"
+   if (currencyselect.value == "Dólar") {
+      currencyname.innerHTML = "Dólar"
       currencyimage.src = "./assets/estados-unidos (1) 1.png"
    }
 
-   if (currencyselect.value == "euro") {
-      currencyname.innerHTML = "euro"
+   if (currencyselect.value == "Euro") {
+      currencyname.innerHTML = "Euro"
       currencyimage.src = "./assets/Euro.png"
+   }
+
+   if (currencyselect.value == "Libra") {
+      currencyname.innerHTML = "Libra"
+      currencyimage.src = "./assets/libra-moeda.png"
+      currencyimage.style.width = "50px";
+   }
+
+   if (currencyselect.value == "Bitcoin") {
+      currencyname.innerHTML = "Bitcoin"
+      currencyimage.src = "./assets/bitcoin-logo.png"
    }
 
    convertValues()
